278c7b1419ffbb1cd8c66d51622bc62e178b4e1c
Gabriel2704 <p_gabriel1999@yahoo.com>
2021-06-03 16:25:23 +0300

    sem 14 part 1 mock

---
+++ Seminar 13/src/ro/ase/cts/tests/TestGrupaWithDummy.java
@@ -0,0 +1,23 @@
+package ro.ase.cts.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ro.ase.cts.clase.Grupa;
+import ro.ase.cts.clase.IStudent;
+import ro.ase.cts.clase.Student;
+
+public class TestGrupaWithDummy {
+
+	@Test
+	public void testAdaugaStudent() {
+		IStudent student = new Student();
+		Grupa grupa = new Grupa(1083);
+		
+		grupa.adaugaStudent(student);
+		
+		assertEquals(1,grupa.getStudenti().size());
+	}
+
+}
---
+++ Seminar 13/src/ro/ase/cts/tests/TestGrupaWithStub.java
@@ -0,0 +1,23 @@
+package ro.ase.cts.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ro.ase.cts.clase.Grupa;
+import ro.ase.cts.clase.IStudent;
+import ro.ase.cts.mock.StudentStub;
+
+public class TestGrupaWithStub {
+
+	@Test
+	public void testPromovabilitateLimitaSuperioara() {
+		Grupa grupa = new Grupa(1083);
+		IStudent student = new StudentStub();
+
+		grupa.adaugaStudent(student);
+
+		assertEquals(1, grupa.getPromovabilitate(), 0.01);
+	}
+
+}
